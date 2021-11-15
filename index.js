713a075d8dfed140b178fdab8b408c7a6fdd8d33
SyedHasibRahman <syedhasib01@gmail.com>
2021-11-16 01:59:42 +0600

    Add Payment Method using stripe

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 const admin = require("firebase-admin");
 const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
+const ObjectId = require('mongodb').ObjectId;
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
 
 // doctors-portal-by-syedhasib-firebase-adminsdk.json
 
@@ -62,12 +65,32 @@ async function run() {
         })
 
 
+        app.get('/appointments/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await appointmentsCollection.findOne(query);
+            res.json(result);
+
+        });
+
         app.post('/appointments', async (req, res) => {
             const appointment = req.body;
             const result = await appointmentsCollection.insertOne(appointment);
 
             // console.log(result);
             res.json(result)
+        });
+        app.put('/appointments/:id', async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    payment: payment
+                }
+            };
+            const result = await appointmentsCollection.updateOne(filter, updateDoc);
+            res.json(result);
         });
         app.get('/users/:email', async (req, res) => {
             const email = req.params.email;
@@ -114,6 +137,16 @@ async function run() {
 
 
 
+        });
+        app.post('/create-payment-intent', async (req, res) => {
+            const paymentInfo = req.body;
+            const amount = paymentInfo.price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                payment_method_types: ['card']
+            });
+            res.json({ clientSecret: paymentIntent.client_secret })
         })
     }
     finally {
